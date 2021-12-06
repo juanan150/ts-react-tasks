9f00a587bce559d9c0a0edf77cda15f2db50ebd2
Juan <juanan0309@gmail.com>
2021-12-06 17:54:19 -0500

    Add basic form and styles with ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type formElement = React.FormEvent<HTMLFormElement>
+type changeElement = React.ChangeEvent<HTMLInputElement>
+
+interface ITask {
+  name: string
+  done: boolean
 }
 
-export default App;
+function App(): JSX.Element {
+  const [newTask, setNewTask] = useState<string>('')
+  const [tasks, setTasks] = useState<ITask[]>([])
+
+  const handleChange = (e: changeElement) => {
+    setNewTask(e.target.value)
+  }
+
+  const handleSubmit = (e: formElement) => {
+    e.preventDefault()
+    addTask(newTask)
+    setNewTask('')
+  }
+
+  const addTask = (name: string) => {
+    setTasks((prevTasks) => [...prevTasks, { name, done: false }])
+  }
+  return (
+    <>
+      <div className="card">
+        <div className="card-body">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="form-control"
+              value={newTask}
+              onChange={(e) => handleChange(e)}
+            />
+            <button type="submit" className="btn btn-primary">
+              Save
+            </button>
+          </form>
+        </div>
+      </div>
+      <ul>
+        {tasks.map((task: ITask, i: number) => (
+          <li key={i + task.name}>{task.name}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default App
